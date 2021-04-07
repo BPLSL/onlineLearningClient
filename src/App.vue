83744778ef28ecfb2f90d5fa54cd4aae3b341f98
BPLSL <419191385@qq.com>
2021-04-07 22:58:45 +0800

    取消ESLint

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
+      APP
+      <br/>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
